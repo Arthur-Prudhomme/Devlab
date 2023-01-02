f6c70ad16f4f48342e2e3c35aeb6abdac1c60272
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-01-02 18:40:55 +0100

    :lipstick: albums lists cover

--- pages/profil.php
+++ pages/profil.php
@@ -1,10 +1,24 @@
 <?php
 require_once '../utils/header.php';
 require_once '../actions/checkLogin.php';
+require_once '../controllers/album.php';
+require_once '../controllers/api.php';
+
 echo '<h1>Hello ' . $_SESSION['user']['username'] . '</h1>';
+
+$album = new Album();
+$api = new API();
+$first_album_id = $album->getFirstAlbumFrom($_SESSION['user']['id'],0);
+$first_album_first_movie_id = $album->getFirstMovieInAlbum($first_album_id);
+$movie = $api->getMovie($first_album_first_movie_id);
+
+echo '<h2>Your Albums</h2>';
+echo '<a href="albums.php"><img src=' . $api->getImg($movie['poster_path'], 200) . '></a><br>';
+
+echo '<h2>Liked Albums</h2>';
+echo '<a href="likedAlbum.php"><img src=https://redellantasonline.com/assets/img-temp/200x300/img1.png></a><br>';
+
 ?>
-<a href="albums.php">Your Albums</a>
-<a href="./likedAlbum.php">Liked Albums</a>
 </body>
 <?php
 require_once '../utils/footer.php';
