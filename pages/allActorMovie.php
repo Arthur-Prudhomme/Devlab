83bc051a48963a184d7474d4a6db8ebdbed9e471
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-01-03 11:12:13 +0100

    :lipstick: Add AllActorMovie Front

--- pages/allActorMovie.php
+++ pages/allActorMovie.php
@@ -6,23 +6,43 @@ $page = $_GET['page'];
 $api = new API();
 
 $actorMovies = $api->getMovieByPerson($person_id, $page);
-foreach ($actorMovies['results'] as $item) {
-    echo '<div>';
-    echo $item['title'] . '<br />';
-    echo '<a href=../pages/movie.php?id=' . $item['id'] . '><img src=' . $api->getImg($item['poster_path'], 200) . '></a>';
-    echo '</div>';
-}
 ?>
-<form method="post">
-    <input type="number" name="page" placeholder="enter page" min="1" max="<?php echo $actorMovies['total_pages'] ?>"
-           value="<?php echo $page ?>">
-    <input type="submit" value="Jump to">
-</form>
+
 <?php
 if (!empty($_POST)) {
     header('Location: allActorMovie.php?id=' . $person_id . '&page=' . $_POST['page']);
 }
 ?>
+
+  <main class="flex flex-col mt-20 lg:mt-32  w-11/12 mx-auto">
+
+    <h2 class="titre uppercase text-rouge mt-8 font-bold text-2xl">Movies</h2>
+
+    <div class="films grid grid-cols-2 lg:grid-cols-5 gap-4 mx-auto mt-8">
+
+        <?php
+
+        foreach($actorMovies['results'] as $item) {
+            echo '<a href=movie.php?id='.$item['id'].'>';
+            echo '<div>';
+            echo '<img src='.$api->getImg($item['poster_path'], 200).'>';
+            echo '<div> </div>';
+            echo '<p>'. $item["title"]. '</p>'. '<br />';
+            echo '</div>';
+            echo '</a>';
+        }
+
+        ?>
+
+    </div>
+
+    <form class="mt-8 mb-24" method="post">
+        <input class="text-fond rounded-md pl-1 w-1/12" type="number" name="page" placeholder="enter page" min="1" max="<?php echo $actorMovies['total_pages'] ?>"
+              value="<?php echo $page ?>">
+        <input class="text-white" type="submit" value="Jump to">
+    </form>
+
+  </main>
 </body>
 <?php
 require_once '../utils/footer.php';
