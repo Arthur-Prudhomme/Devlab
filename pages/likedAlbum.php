dbc418e16b049d7c960106d13ec4d31b4097a87d
arthur <arthurprudhommemail06@gmail.com>
2023-01-05 23:32:38 +0100

    :sparkles: can see users liked albums

--- pages/likedAlbum.php
+++ pages/likedAlbum.php
@@ -8,25 +8,42 @@ $connection = new Connection();
 $album = new Album();
 $api = new API();
 
-echo '<h2>The Albums you liked</h2>';
-
-$likedAlbums = $album->getAllLikedAlbumsFromUser($_SESSION['user']['id']);
-
-foreach ($likedAlbums as $albums) {
-    $movie_id = $album->getFirstMovieInAlbum($albums['album_id']);
-    if (isset($movie_id)) {
-        $movie = $api->getMovie($movie_id);
-        $album_cover = $api->getImg($movie['poster_path'], 200);
+if(!isset($_GET['username'])){
+    echo '<h2>The Albums you liked</h2>';
+    $likedAlbums = $album->getAllLikedAlbumsFromUser($_SESSION['user']['id']);
+}else {
+    if ($connection->getUserByUsername($_GET['username'], 0) == null) {
+        $no_user = 1;
+        echo '<h1>No user found with the name "' . $_GET['username'] . '"</h1>';
     } else {
-        $album_cover = 'https://redellantasonline.com/assets/img-temp/200x300/img1.png';
+        echo '<h2>The Albums that '.$_GET['username'].' liked</h2>';
+        $userId = $connection->getUserIdByUsername($_GET['username']);
+        $likedAlbums = $album->getAllLikedAlbumsFromUser($userId);
     }
-    echo '<div id="l' . $albums['album_id'] . '">';
-    $album_infos = $album->getAlbumInfosById($albums['album_id']);
-    $user_infos = $connection->getUserById($album_infos[1]);
-    echo $album_infos[2].' from '.$user_infos[1]. '<br />';
-    echo '<a href="albumContent.php?id=' . $albums['album_id'] . '"><img src=' . $album_cover . '></a><br>';
-    echo '<button onclick=likeAlbum(' . $albums['album_id'] . ',' . $_SESSION['user']['id'] . ')>👍</button>';
-    echo '</div>';
+}
+
+if(!isset($no_user)) {
+    foreach ($likedAlbums as $albums) {
+        $movie_id = $album->getFirstMovieInAlbum($albums['album_id']);
+        if (isset($movie_id)) {
+            $movie = $api->getMovie($movie_id);
+            $album_cover = $api->getImg($movie['poster_path'], 200);
+        } else {
+            $album_cover = 'https://redellantasonline.com/assets/img-temp/200x300/img1.png';
+        }
+        echo '<div id="l' . $albums['album_id'] . '">';
+        $album_infos = $album->getAlbumInfosById($albums['album_id']);
+        $user_infos = $connection->getUserById($album_infos[1]);
+        echo $album_infos[2] . ' from ' . $user_infos[1] . '<br />';
+        if(isset($_GET['username'])){
+            $_SESSION['exploreUsername'] = $user_infos[1];
+        }
+        echo '<a href="albumContent.php?id=' . $albums['album_id'] . '"><img src=' . $album_cover . '></a><br>';
+        if(!isset($_GET['username'])) {
+            echo '<button onclick=likeAlbum(' . $albums['album_id'] . ',' . $_SESSION['user']['id'] . ')>👍</button>';
+        }
+        echo '</div>';
+    }
 }
 
 ?>
