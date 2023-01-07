1060f593b94b7595bf7d92d2e4b90daee2057a4d
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-01-07 20:47:10 +0100

    :lipstick: Add Favicon

--- utils/header.php
+++ utils/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="../build/img/logo_v2_fond.svg" type="image/x-icon">
   <link href="./style.css" rel="stylesheet">
   <link href="../style.css" rel="stylesheet">
   <link href="src/input.css" rel="stylesheet">
