74dca90f4d9773cca32c411e1928119ee85199c0
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-01-08 14:46:12 +0100

    :lipstick: Update Profil Page

--- pages/profil.php
+++ pages/profil.php
@@ -36,7 +36,7 @@
         <div class="lg:w-1/2 w-full flex flex-row relative">
           <?php
             echo '<input class="w-full mt-6 lg:mt-0 h-1/4 bg-transparent text-gris border border-gris p-1 px-2 rounded" id="user_search_bar" placeholder="Search User" name="input" oninput=instantResearch("../sources/dynamicUserSearch.php",1,0,0) />';
-            echo '<ul class=" absolute flex flex-col pt-8 p-3 rounded top-0 -z-10" id="user_search_results"></ul>';
+            echo '<ul class=" absolute flex flex-col pt-8 p-3 rounded top-0 z-50" id="user_search_results"></ul>';
           ?> 
         </div>
       </div>
@@ -82,22 +82,45 @@
       </div>
     <?php
     }else{
-      if($connection->getUserByUsername($_GET['username'],0,'') == null){
-          echo '<h1>No user found with the name "'.$_GET['username'].'"</h1>';
-      }else{
-          echo '<h1>'.$_GET['username'].'\'s Profil</h1>';
-
-          echo '<h2>'.$_GET['username'].'\'s Albums</h2>';
-          echo '<a href="albums.php?username='.$_GET['username'].'"><img src=https://redellantasonline.com/assets/img-temp/200x300/img1.png></a><br>';
-
-          echo '<h2>'.$_GET['username'].'\'s Liked Albums</h2>';
-          echo '<a href="likedAlbum.php?username='.$_GET['username'].'"><img src=https://redellantasonline.com/assets/img-temp/200x300/img1.png></a><br>';
-      }
-    }
-
       ?>
-    </div>
-    </div>
+      <div class="w-full h-[65vh] mx-auto mt-6">
+        <?php
+            if($connection->getUserByUsername($_GET['username'],0,'') == null){
+                echo '<h1 class="uppercase font-bold mt-8 lg:mt-4 text-2xl lg:text-3xl lg:mb-8 mb-2">No user found with the name "'.$_GET['username'].'"</h1>';
+            }else{
+              echo '<h1 class="uppercase font-bold mt-8 lg:mt-4 text-2xl lg:text-3xl lg:mb-8 mb-2">'.$_GET['username'].'\'s Profil</h1>';
+              ?>
+              <div class="w-full h-[75vh] grid grid-cols-2 gap-4 mx-auto mt-6">
+                <div class="flex flex-col items-center ">
+                  <?php
+                    echo '<div class="flex flex-col items-center">';
+                      echo '<div class="flex flex-col items-center text-center relative w-full">';
+                        echo '<a class="absolute w-[31vh] h-[47vh] z-10 bottom-0" href="albums.php?username='.$_GET['username'].'"></a>';
+                        echo '<p class="absolute text-white font-bold text-base z-10 w-11/12 bottom-5">'.$_GET['username'].'\'s Albums</p>';
+                        echo '<a class="mt-4 w-[31vh] h-[47vh] ouaip" href="albums.php?username='.$_GET['username'].'"><img class="rounded-lg lg:w-[31vh] lg:h-[47vh] " src=https://redellantasonline.com/assets/img-temp/200x300/img1.png></a>';
+                        echo '<div class="w-[31vh] h-[47vh] absolute gradient"></div>';
+                      echo '</div>';
+                    echo '</div>';
+                  ?> 
+                </div>
+                <div class="flex flex-col items-center ">
+                  <?php
+                    echo '<div class="flex flex-col items-center">';
+                      echo '<div class="flex flex-col items-center text-center relative w-full">';
+                        echo '<a class="absolute w-[31vh] h-[47vh] z-10 bottom-0" href="likedAlbum.php?username='.$_GET['username'].'"></a>';
+                        echo '<p class="absolute text-white font-bold text-base z-10 w-11/12 bottom-5">'.$_GET['username'].'\'s Liked Albums</p>';
+                        echo '<a class="mt-4 w-[31vh] h-[47vh] ouaip" href="likedAlbum.php?username='.$_GET['username'].'"><img class="rounded-lg lg:w-[31vh] lg:h-[47vh] " src=https://redellantasonline.com/assets/img-temp/200x300/img1.png></a>';
+                        echo '<div class="w-[31vh] h-[47vh] absolute gradient"></div>';
+                      echo '</div>';
+                    echo '</div>';
+                      }
+                    }
+                  ?>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
